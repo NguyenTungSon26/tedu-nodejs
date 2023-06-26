9bd35f2af932cd8517f23a3f7487016dae954103
NguyenTungSon26 <@NguyenTungSon26>
2023-06-26 16:41:44 +0700

    middlewave update

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,27 @@
-import { Route } from 'core/interfaces';
+import { Route } from './core/interfaces';
 import express  from 'express';
+import mongoose from 'mongoose';
+import hpp from "hpp"
+import morgan from "morgan"
+import cors from "cors"
+import helmet from "helmet"
+import { Logger } from './core/utils';
 class App {
     public app: express.Application
     public port : string | number; 
+    public production: boolean 
 
     constructor(routes:Route[]){
         this.app = express();
         this.port = process.env.PORT || 5000
-
+        this.production = process.env.NODE_ENV == "production" ? true : false
         this.initializeRoutes(routes)
+        this.connectToDatabase()
     }
 
     public listen(){
         this.app.listen(this.port,()=>{
-            console.log(`Server running on port ${this.port}`);
+            Logger.info(`Server running on port ${this.port}`);
             
         })
     }
@@ -22,8 +30,31 @@ class App {
         routes.forEach((route)=>{
             this.app.use("/",route.router);
         })
-
+    
     }
-}
+    private initializeMiddlewares(){
+        if(this.production){
+            this.app.use(hpp())
+            this.app.use(helmet())
+            this.app.use(morgan("combined"))
+            this.app.use(cors({origin: "your.domain.com", credentials:true}))
+        } else {
+            this.app.use(morgan("dev"))
+            this.app.use(cors({origin:true,credentials:true}))
+        }
+    }
+
+    private connectToDatabase(){
+            const connectString = process.env.MONGODB_URI;
+            if(!connectString){
+                Logger.error("Connection string is invalid")
+                return;
+            }
+            mongoose.connect(connectString). catch ((reason)=>{
+            Logger.error(reason)
+        });
+        Logger.info("Database connected...")
+        }
+    }
 
 export default App
